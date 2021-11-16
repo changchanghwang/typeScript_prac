6005b82d1e9e53497ffd6cd5bcd94a9fa2268456
CH_Hwang <hch950627@naver.com>
2021-11-17 02:29:39 +0900

    [refactor] middleware method 추가

--- type_express sequelize/src/app.ts
+++ type_express sequelize/src/app.ts
@@ -1,19 +1,24 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
+import * as helmet from 'helmet';
 import 'dotenv/config';
 import indexRouter from './routes/index';
 
 class Server {
-  private App: express.Application;
+  private app: express.Application;
   constructor() {
-    this.App = express();
-    this.App.use(express.urlencoded({ extended: true }));
-    this.App.use(express.json());
-    this.App.use(morgan('dev'));
-    this.App.use('/', indexRouter);
+    this.app = express();
+    this.initializeMiddlewares();
+    this.app.use('/', indexRouter);
   }
   public getInstance(): express.Application {
-    return this.App;
+    return this.app;
+  }
+  private initializeMiddlewares() {
+    this.app.use(morgan('dev'));
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
   }
 }
 
